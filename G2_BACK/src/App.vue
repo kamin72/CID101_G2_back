8156731538dba4f3080297bb29bf4a28bb4978b0
l11y0 <leeyo.yh@gmail.com>
2024-06-12 16:50:06 +0800

    完成login畫面

--- G2_BACK/src/App.vue
+++ G2_BACK/src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <RouterView />
-  <div>123</div>
-  <TestView />
 </template>
 
 <script>
 import { RouterView } from 'vue-router'
-import TestView from '@/components/TestView.vue'
 
 export default {
   components: {
     RouterView,
-    TestView
   }
 }
 </script>
