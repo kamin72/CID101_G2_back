33408e30f1eaa9e946338f5e99f9e4cc89ac20fd
kamin72 <a1616001@gmail.com>
2024-06-14 21:05:35 +0800

    add

--- G2_BACK/src/components/ordermanage/OrderCount.vue
+++ G2_BACK/src/components/ordermanage/OrderCount.vue
@@ -1,6 +1,54 @@
 <template>
   <div class="d-flex justify-content-between">
     <div class="mt-3 w-50">
+      <label for="access" class="form-label fw-bolder">類別</label>
+      <select class="form-select" aria-label="Default select example">
+        <option selected>類別</option>
+        <option value="紅酒">紅酒</option>
+        <option value="粉紅酒">粉紅酒</option>
+        <option value="加烈酒">加烈酒</option>
+        <option value="白酒">白酒</option>
+        <option value="氣泡酒">氣泡酒</option>
+      </select>
+    </div>
+    <div class="mt-3 w-50">
+      <label for="access" class="form-label fw-bolder">品種</label>
+      <select class="form-select" aria-label="Default select example">
+        <option selected>來源</option>
+        <option value="波爾多混釀">波爾多混釀</option>
+        <option value="黑皮諾">黑皮諾</option>
+        <option value="帕洛米諾">帕洛米諾</option>
+        <option value="阿里戈蝶">阿里戈蝶</option>
+        <option value="夏多內">夏多內</option>
+      </select>
+    </div>
+    <div class="mt-3 w-50">
+      <label for="access" class="form-label fw-bolder">商品名稱</label>
+      <select class="form-select" aria-label="Default select example">
+        <option selected>商品名稱</option>
+        <option value="批發商">批發商</option>
+        <option value="一般會員">一般會員</option>
+      </select>
+    </div>
+  </div>
+  <div class="d-flex justify-content-between mt-4">
+    <div class="input-group z-0 w-auto h-25 align-self-end">
+      <input
+        type="date"
+        class="form-control"
+        placeholder=""
+        aria-label="Example text with two button addons"
+      />
+      <span class="ms-3 me-3">~</span>
+      <input
+        type="date"
+        class="form-control"
+        placeholder=""
+        aria-label="Example text with two button addons"
+      />
+      <button class="btn btn-outline-primary" type="button">搜尋</button>
+    </div>
+    <div class="mt-3 w-50">
       <label for="access" class="form-label fw-bolder">來源</label>
       <select class="form-select" aria-label="Default select example">
         <option selected>來源</option>
@@ -8,16 +56,8 @@
         <option value="一般會員">一般會員</option>
       </select>
     </div>
-    <div class="input-group z-0 w-auto h-25 align-self-end">
-      <input
-        type="text"
-        class="form-control"
-        placeholder=""
-        aria-label="Example text with two button addons"
-      />
-      <button class="btn btn-outline-primary" type="button">搜尋</button>
-    </div>
   </div>
+
   <table class="table mt-5">
     <thead>
       <tr>
